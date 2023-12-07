5f73f74a89530f25277924e5522570709d369986
ThomDlAOC <>
2023-12-07 22:09:40 +0100

    update template

--- day_template/main.cpp
+++ day_template/main.cpp
@@ -16,15 +16,15 @@
  * Setup code
  **************/
 
-void part1(std::stringstream &file_content);
-void part2(std::stringstream &file_content);
+std::string part1(std::stringstream &file_content);
+std::string part2(std::stringstream &file_content);
 
 std::optional<std::stringstream> readFileContent(const std::string &path)
 {
     std::ifstream file(path);
     if (!file.is_open())
     {
-        std::cout << "Could not open file " << path << std::endl;
+        std::cerr << "Could not open file " << path << std::endl;
         return std::nullopt;
     }
 
@@ -35,37 +35,59 @@ std::optional<std::stringstream> readFileContent(const std::string &path)
     return std::move(file_content);
 };
 
-void executeFunction(std::stringstream &file_content, std::function<void(std::stringstream &)> function)
+void executeFunction(std::stringstream &file_content, std::function<std::string(std::stringstream &)> function, std::string label, std::string expected)
 {
+
     file_content.clear();
     file_content.seekg(0, std::ios::beg);
     std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
-    function(file_content);
+    std::string result = function(file_content);
     std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
-    std::cout << "Execution time: " << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() << "ms" << std::endl;
+    if (result != expected)
+    {
+        std::cerr << "\033[1;31m" << label << " failed. Expected " << expected << " but got " << result << "\033[0m" << std::endl;
+    }
+    else
+    {
+        std::cout << "\033[1;32m" << label << " result: " << result << "\033[0m" << std::endl;
+    }
+    std::cerr << "Execution time: " << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() << "ms" << std::endl;
 }
 
 int main(int argc, char const *argv[])
 {
-    if (argc != 2)
+    if (argc != 3)
     {
-        std::cout << "Usage: " << argv[0] << " <path>" << std::endl;
+        std::cerr << "Usage: " << argv[0] << " <path> <expected_path>" << std::endl;
         return 1;
     }
 
     std::string path = argv[1];
+    std::string expected_path = argv[2];
     std::optional<std::stringstream> file_content = readFileContent(path);
+    std::optional<std::stringstream> file_expected_content = readFileContent(expected_path);
 
     if (!file_content)
     {
-        std::cout << "Could not read file " << path << std::endl;
+        std::cerr << "Could not read file " << path << std::endl;
+        return 1;
+    }
+
+    if (!file_expected_content)
+    {
+        std::cerr << "Could not read file " << expected_path << std::endl;
         return 1;
     }
 
     std::stringstream &file_content_ref = *file_content;
+    std::stringstream &file_expected_content_ref = *file_expected_content;
 
-    executeFunction(file_content_ref, part1);
-    executeFunction(file_content_ref, part2);
+    std::string expected;
+
+    file_expected_content_ref >> expected;
+    executeFunction(file_content_ref, part1, "Part 1", expected);
+    file_expected_content_ref >> expected;
+    executeFunction(file_content_ref, part2, "Part 2", expected);
 
     return 0;
 }
@@ -75,12 +97,12 @@ int main(int argc, char const *argv[])
  **************************************/
 
 
-void part1(std::stringstream &file_content)
+std::string part1(std::stringstream &file_content)
 {
 
 }
 
-void part2(std::stringstream &file_content)
+std::string part2(std::stringstream &file_content)
 {
     
 }
